20da9c59d6c852ce94c9db57c0cb0341d38ad741
capturewiz <startup@pixelmetrics.com>
2013-08-15 07:07:38 -0600

    Update init.el to show line numbers in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,3 +75,8 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; ---------------------------
+;; -- My customizations --
+;; ---------------------------
+(global-linum-mode t)
